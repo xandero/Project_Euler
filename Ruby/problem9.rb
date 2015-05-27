579edc43466922fe6a8bd2e63d2aa2829b6c924b
xandero <alexander.ohan@gmail.com>
2015-05-27 19:50:34 +1000

    problem 9

--- Ruby/problem9.rb
+++ Ruby/problem9.rb
@@ -1,2 +1,16 @@
 # a + b + c == 1000 && a < b < c && a**2 + b**2 == c**2
 
+product = 0
+ 
+1.upto(1000) { |a|  
+  (a+1).upto(1000) { |b|
+    c = 1000 - a - b
+    if (a*a + b*b == c*c)
+       product = a*b*c
+       break
+    end   
+  }
+  break if product > 0
+}
+ 
+puts product
